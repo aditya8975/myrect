929e79bc50b0cf46ab04ede1cc998ad16faee2bf
adityakatare301 <32974391-adityakatare301@users.noreply.replit.com>
2024-04-21 11:36:05 +0000

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
   return (
     <main>
-<h1>Hello World</h1>
+
     </main>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom'
+// import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
+// var name="Aditya";
+// const currD=new Date();
+// var current=2019;
+// ReactDOM.render(
+//   <div>  
+//   <h1>craated by {name}</h1>
+//     <h2>copyright {currD.getFullYear()}</h2>
+// </div>
+//   ,document.getElementById('root')
+// );
+const img="https://picsum.photos/200";
+ReactDOM.render(
+  <div>
+    <h1 className="title">My Faviroute food</h1>
+   <div className='box'>
+     <img src={img} />
+     <img src={img} />
+     <img src={img} />
+     <img src={img} />
+  </div>
+  </div>
+  ,document.getElementById('root')
+);
+
+
+
+
+
